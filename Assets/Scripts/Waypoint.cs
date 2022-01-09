8b64997aac44ffabc657b247e8a2bc4b0e2b7d6b
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-01-08 22:45:03 -0800

    add ballista with click

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] bool isPlaceable;
+    [SerializeField] GameObject ballista;
 
     private void OnMouseDown()
     {
@@ -12,6 +13,6 @@ public class Waypoint : MonoBehaviour
         {
             Vector2 currentTile = new Vector2(this.transform.position.x, this.transform.position.z);
             print(currentTile / 10);
-        }
+            Instantiate(ballista, transform.position, Quaternion.identity);        }
     }
 }
