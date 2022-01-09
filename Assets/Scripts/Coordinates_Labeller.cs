1ce2956932aae1b43b94da025050ea71cfe470b1
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-01-08 18:36:00 -0800

    coordinates labeller

--- Assets/Scripts/Coordinates_Labeller.cs
+++ Assets/Scripts/Coordinates_Labeller.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
+[ExecuteAlways]
 public class Coordinates_Labeller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    TextMeshPro label;
+    Vector2Int coordinates;
+
+    private void Awake()
     {
-        
+        label = GetComponent<TextMeshPro>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!Application.isPlaying)
+        {
+            DisplayCoordinates();
+        }
+    }
+
+    private void DisplayCoordinates()
+    {
+        coordinates.x = (int)transform.position.x / (int)UnityEditor.EditorSnapSettings.move.x;
+        coordinates.y = (int)transform.position.z / (int)UnityEditor.EditorSnapSettings.move.z;
+        label.text = coordinates.x + "," + coordinates.y;
+        transform.parent.name = coordinates.x.ToString() + "," + coordinates.y.ToString();
     }
 }
