5e73e0050ec27aefbe7a25f1a7dcf9d3cc18439f
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-01-09 08:51:00 -0800

    add target location
    add ballista firing

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -13,6 +13,8 @@ public class Waypoint : MonoBehaviour
         {
             Vector2 currentTile = new Vector2(this.transform.position.x, this.transform.position.z);
             print(currentTile / 10);
-            Instantiate(ballista, transform.position, Quaternion.identity);        }
+            Instantiate(ballista, transform.position, Quaternion.identity);
+            isPlaceable = false;
+        }
     }
 }
