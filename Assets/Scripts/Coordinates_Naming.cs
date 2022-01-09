dbdf560fea4a8d1cef341d1aa00315ec6dd18ff8
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-01-08 17:15:36 -0800

    Initial commit

---
+++ Assets/Scripts/Coordinates_Naming.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Coordinates_Naming : MonoBehaviour
+{
+    private void Awake()
+    {
+        print("i'm awake");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
