e74c4f6b288bf1a0dc2ef8f5d1c18128a871c754
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-01-09 22:03:09 -0800

    fix targeting script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] Transform weapon;
-    Transform target;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -17,32 +14,8 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FindClosestTarget();
 
     }
 
-    private void FindClosestTarget()
-    {
-        Enemy[] enemies = FindObjectsOfType<Enemy>();
-        Transform closestTarget = null;
-        float maxDistance = Mathf.Infinity;
-
-        foreach(Enemy enemy in enemies)
-        {
-            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
-
-            if(targetDistance<maxDistance)
-            {
-                closestTarget = enemy.transform;
-                maxDistance = targetDistance;
-            }
-        }
-
-        target = closestTarget;
-    }
-
-    void AimWeapon()
-    {
-        weapon.LookAt(target);
-    }
+    
 }
--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] float range = 15f;
     [SerializeField] Transform target;
 
 
@@ -20,9 +21,32 @@ public class TargetLocator : MonoBehaviour
         AimWeapon();
     }
 
+    private void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
+    }
+
+
     private void AimWeapon()
     {
-        transform.LookAt(target);
+        float targetDistance = Vector3.Distance(transform.position, target.position);
+
+        weapon.LookAt(target);
     }
 
 }
